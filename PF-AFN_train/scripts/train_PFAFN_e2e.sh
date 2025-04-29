d9639e38f6cb19081a25453313749d56764f6de8
diixo <anatoliy.podoba@dxc.com>
2025-04-29 22:39:59 +0300

    sh-s coding style

--- PF-AFN_train/scripts/train_PFAFN_e2e.sh
+++ PF-AFN_train/scripts/train_PFAFN_e2e.sh
@@ -3,12 +3,3 @@ python -m torch.distributed.launch --nproc_per_node=8 --master_port=7129 train_P
 --PBAFN_warp_checkpoint 'checkpoints/PBAFN_e2e/PBAFN_warp_epoch_101.pth' --PBAFN_gen_checkpoint 'checkpoints/PBAFN_e2e/PBAFN_gen_epoch_101.pth'  \
 --resize_or_crop None --verbose --tf_log --batchSize 4 --num_gpus 8 --label_nc 14 --launcher pytorch
 
-
-
-
-
-
-
-
-
-
